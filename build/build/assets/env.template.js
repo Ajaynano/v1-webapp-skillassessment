e6423d614ead9fdeab273b3ce44479021a50ad2d
Ajay Raghu <Ajay.Raghu@bmwtechworks.in>
2025-09-22 21:44:44 +0530

    added code

---
+++ build/build/assets/env.template.js
@@ -0,0 +1,6 @@
+(function(window) {
+    window.__env = window.__env || {};
+    window.__env.API_URL = "${API_URL}";
+    window.__env.LP_AI_API_URL = "${LP_AI_API_URL}";
+    window.__env.LP_API_URL = "${LP_API_URL}";
+})(this);
