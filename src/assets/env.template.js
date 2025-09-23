1ee6956409f0644eb6c950cb009fba77470b849a
Ajay Raghu <Ajay.Raghu@bmwtechworks.in>
2025-09-23 11:54:05 +0530

    working code

--- src/assets/env.template.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-    window.__env = window.__env || {};
-    window.__env.API_URL = "${API_URL}";
-    window.__env.LP_AI_API_URL = "${LP_AI_API_URL}";
-    window.__env.LP_API_URL = "${LP_API_URL}";
-})(this);
